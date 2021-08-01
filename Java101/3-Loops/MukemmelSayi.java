b495416adc71609b3f97a71540f38c4aa045e9d8
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-02 00:00:06 +0300

    hw added
    perfect number created

---
+++ Java101/3-Loops/MukemmelSayi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import jdk.nashorn.api.tree.WhileLoopTree;
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        int number,modNumber = 0,sum = 0;
+        Scanner getInput = new Scanner(System.in);
+        System.out.print("Sayi Giriniz : ");
+        number = getInput.nextInt();
+        getInput.close();
+        while(modNumber != number-1){
+            modNumber++;
+            if(number%modNumber == 0){
+                sum +=modNumber;
+            }
+        }
+        if(sum == number){
+            System.out.println( number + " Mükemmel sayıdır");
+        }else{
+            System.out.println(number + " Mükemmel sayı değildir");
+        }
+    }
+}
