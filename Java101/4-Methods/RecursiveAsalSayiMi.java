dbd086be8387448e42b725d30a81dd0616f7d6a6
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-04 15:54:17 +0300

    hw done
    recursivly find isPrime

---
+++ Java101/4-Methods/RecursiveAsalSayiMi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RecursiveAsalSayiMi {97
+    static boolean isPrime(int number,int half){
+        if(half == 1){
+            return true;
+        }
+        if(number<2){
+            return false;
+        }
+        return ( !(number % half == 0) && isPrime(number,half-1));
+    }
+    public static void main(String[] args) {
+        Scanner getInput  = new Scanner(System.in);
+        System.out.print("Sayı Giriniz: ");
+        int number = getInput.nextInt();
+        getInput.close();
+        if(isPrime(number, number/2)){
+            System.out.print("Asal");
+
+        }else{
+            System.out.print("Asal değil");
+        }  
+    }
+}
