c9b36d4ce74718cca9793616f0227c08f5a9c6ff
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-07 02:03:51 +0300

    hw done
    harmonicaverage

---
+++ Java101/6-Arrays/HarmonikOrtalama.java
@@ -0,0 +1,25 @@
+
+
+public class HarmonikOrtalama {
+    
+    static double harmonic(int arr[]){
+       double sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i]/(i+1);
+        }
+        return (double)(arr.length/(double)sum);
+    }
+    
+    public static void main(String[] args) {
+        ///////////////////////////////////////////////
+        int[] arr = new int[10];
+        int random;
+        for (int i = 0; i<arr.length; i++) {
+          random = (int) (Math.random()*100); //fill array with random integer numbers
+          arr[i] = random;
+        } 
+        //////////////////////////////////////////////
+        System.out.println("Harmonik Ortalama : " + harmonic(arr));
+
+    }
+}
