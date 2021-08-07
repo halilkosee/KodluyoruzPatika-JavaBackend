34612c06bc8713cc6169214ea327f938d0ceebc2
halilkosee <51232723+halilkosee@users.noreply.github.com>
2021-08-07 03:26:40 +0300

    hw
    nearest number belongs to input done

---
+++ Java101/6-Arrays/EnYakin.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EnYakin {
+    public static void main(String[] args) {
+        ///////////////////////////////////////////////
+        int[] arr = new int[10];
+        int random;
+        for (int i = 0; i<arr.length; i++) {
+          random = (int) (Math.random()*10); //fill array with random integer numbers
+          arr[i] = random;
+        } 
+        System.out.println("Dizi :" + Arrays.toString(arr));
+        //////////////////////////////////////////////
+        Scanner getInput = new Scanner(System.in);
+        int low = 0,high = 0;
+        System.out.println();
+        System.out.print("Girilen sayi : " );
+        int input = getInput.nextInt();
+        getInput.close();
+        Arrays.sort(arr);
+        if(arr[0] == input){
+            low = 0;
+            high = arr[1];
+        }else if(arr[9]==input){
+            low = arr[8];
+            high = 0;
+
+        }else{7
+            for(int i = 0; i<arr.length;i++){
+                if(arr[i]==input){
+                    low = arr [i -1];
+                    high = arr[i+ 1];
+                    break;  
+                }
+            }
+        }
+        System.out.println("Girilen sayıdan küçük en yakın sayı : " + low);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : " + high);
+    }
+    
+}
--- Java101/6-Arrays/HarmonikOrtalama.java
+++ Java101/6-Arrays/HarmonikOrtalama.java
@@ -1,5 +1,3 @@
-
-
 public class HarmonikOrtalama {
     
     static double harmonic(int arr[]){
